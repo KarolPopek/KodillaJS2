849b399ed263f4f752c51ca68090a02ce4ef5f48
KarolPopek <karol.popek@wp.pl>
2017-02-21 18:22:38 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// Deklaracja zmiennej i umieszczenie w niej funkcji wyrzucającej na ekran tekst i pole tesktowe
+var name = prompt('Wpisz swoje imie:');
+
+// Alert
+//alert('Witaj, ' + name);
+
+// Napis w konsoli
+console.log('Witaj, ' + name);
